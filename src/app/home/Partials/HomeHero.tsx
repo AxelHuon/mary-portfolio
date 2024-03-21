b77817793307ee68668f4b49ca3b39161e47827b
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2024-03-21 21:39:15 +0100

    [feat/hero-home][feat]:add some anim text

--- src/app/home/Partials/HomeHero.tsx
+++ src/app/home/Partials/HomeHero.tsx
@@ -1,38 +1,92 @@
 'use client';
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import SectionXL from '@/components/Atomes/Sections/SectionXL/SectionXL';
 import TextStyled from '@/components/Atomes/TextStyled/TextStyled';
+import { useGSAP } from '@gsap/react';
+import { SplitText } from 'gsap/SplitText';
+import gsap from 'gsap';
+import { Colors } from '@/theme/colors';
 
+gsap.registerPlugin(SplitText);
 const Container = styled.aside`
   height: 100vh;
   width: 100%;
 `;
-export const ContainerTitle = styled.div`
+const ContainerTitle = styled.div`
   display: flex;
+  padding-top: 150px;
   flex-direction: column;
 `;
-export const ContainerTitleBR = styled.div`
+const ContainerTitleBR = styled.div`
   display: flex;
   justify-content: center;
+  opacity: 0;
+  h2 {
+    div {
+      opacity: 0;
+      transform: translateY(100px);
+    }
+  }
 `;
-export const ContainerTitleMBT = styled.div`
+const ContainerTitleMBT = styled.div`
   display: flex;
   justify-content: flex-end;
   transform: translateX(-20px);
+  opacity: 0;
 `;
 const HomeHero: React.FC = () => {
+  const TitleBrandDesignRef = useRef<HTMLDivElement>(null);
+  const ContainerMaryImageRef = useRef<HTMLDivElement>(null);
+
+  /*Animation Image*/
+  useGSAP(
+    () => {
+      gsap.to(ContainerMaryImageRef.current, {
+        duration: 2,
+        delay: 2,
+        width: '90%',
+        top: '65%',
+        height: '508px',
+      });
+    },
+    { scope: ContainerMaryImageRef },
+  );
+
+  /*Animation Text*/
+  useGSAP(
+    () => {
+      const designBrandSplitText = new SplitText('h2', {
+        type: 'chars',
+      });
+      gsap.to(TitleBrandDesignRef.current, {
+        duration: 0,
+        delay: 0.1,
+        opacity: 1,
+      });
+      gsap.to(designBrandSplitText.chars, {
+        duration: 0.6,
+        y: 0,
+        delay: 2,
+        opacity: 1,
+        stagger: 0.05,
+        ease: 'power.inOut',
+      });
+    },
+    { scope: TitleBrandDesignRef },
+  );
+
   return (
     <SectionXL>
       <Container>
         <ContainerTitle>
-          <ContainerTitleBR>
+          <ContainerTitleBR ref={TitleBrandDesignRef}>
             <TextStyled as={'h2'} $lineHeight={'120px'} fontSize={'200px'} fontFamily={'Nanum Pen'}>
               BRAND DESIGNER
             </TextStyled>
           </ContainerTitleBR>
           <ContainerTitleMBT>
-            <TextStyled as={'h1'} fontSize={'40px'} fontFamily={'Nanum Pen'}>
+            <TextStyled color={Colors.PRIMARY} as={'h1'} fontSize={'40px'} fontFamily={'Nanum Pen'}>
               MARY BONNANCY THOMAS
             </TextStyled>
           </ContainerTitleMBT>
---
+++ src/data/gallery.ts
@@ -0,0 +1,43 @@
+export interface galleryItem {
+  id: number;
+  url: string;
+  alt: string;
+}
+
+export const dataGallery: galleryItem[] = [
+  {
+    id: 1,
+    url: 'gallery1',
+    alt: 'gallery1',
+  },
+  {
+    id: 2,
+    url: 'gallery2',
+    alt: 'gallery2',
+  },
+  {
+    id: 3,
+    url: 'gallery3',
+    alt: 'gallery3',
+  },
+  {
+    id: 4,
+    url: 'gallery4',
+    alt: 'gallery4',
+  },
+  {
+    id: 5,
+    url: 'gallery5',
+    alt: 'gallery5',
+  },
+  {
+    id: 6,
+    url: 'gallery6',
+    alt: 'gallery6',
+  },
+  {
+    id: 7,
+    url: 'mary',
+    alt: 'mary',
+  },
+];
