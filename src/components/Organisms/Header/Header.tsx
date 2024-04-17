78c31d33b0463b3fbb03e06139d51c835c5d1695
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2024-04-17 12:26:56 +0200

    [feat/add-new-menu][feat]:add responsive

--- src/components/Organisms/Header/Header.tsx
+++ src/components/Organisms/Header/Header.tsx
@@ -16,7 +16,7 @@ const HeaderContainer = styled.header`
   position: fixed;
   top: 0;
   width: 100%;
-  z-index: 20000;
+  z-index: 9999;
 `;
 const NavigationContainer = styled.nav`
   display: flex;
@@ -43,65 +43,20 @@ const NavigationContainer = styled.nav`
 const Header: React.FC = () => {
   const headerContainerRef = useRef<HTMLDivElement>(null);
   const { contextSafe } = useGSAP({ scope: headerContainerRef }); // we can just pass in a config object as the 1st parameter to make scoping simple
-  const [timeLineState, setTimeLineState] = useState<gsap.core.Timeline | null>(null);
+  const [timeLineMenu, setTimeLineMenu] = useState<gsap.core.Timeline | null>(null);
 
   useGSAP(() => {
     CustomEase.create('custom', 'M0,0 C0.85,0 0.2,1 1,1');
-    const timeline = gsap.timeline({ paused: true });
-    timeline.to('.bg__button__bar__one', {
-      top: '50%',
-      rotate: -45,
-      ease: 'custom',
-      duration: 0.5,
-    });
-    timeline.to(
-      '.bg__button__bar__two',
-      { top: '50%', rotate: 45, ease: 'custom', duration: 0.3 },
-      '>-0.5',
-    );
-    timeline.to('.menu', { top: 0, ease: 'custom', duration: 1.4 }, '>-0.3');
-    timeline.fromTo(
-      '.menu li',
-      { y: 100, opacity: 0 },
-      { y: 0, opacity: 1, ease: 'expo.out', duration: 1, stagger: 0.2 },
-      '>-0.3',
-    );
-    timeline.to(
-      '.menu__image__one',
-      {
-        clipPath: 'polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)',
-        ease: 'custom',
-        duration: 0.7,
-      },
-      '>-1.5',
-    );
-    timeline.to(
-      '.menu__image__two',
-      {
-        clipPath: 'polygon(0% 0%, 0% 100%, 100% 100%, 100% 0%)',
-        ease: 'custom',
-        duration: 0.7,
-      },
-      '>-0.2',
-    );
-    timeline.fromTo(
-      '.container__about div',
-      {
-        y: 30,
-        opacity: 0,
-      },
-      { opacity: 1, y: 0, ease: 'expo.out', duration: 0.8, stagger: 0.1 },
-      '>-0.7',
-    );
-    setTimeLineState(timeline);
+    const timelineMenu = gsap.timeline({ paused: true });
+    setTimeLineMenu(timelineMenu);
   });
 
   const openMenu = contextSafe(() => {
-    if (timeLineState) {
-      if (timeLineState.progress() === 0) {
-        timeLineState.play();
-      } else if (timeLineState.progress() === 1) {
-        timeLineState.reverse(0);
+    if (timeLineMenu) {
+      if (timeLineMenu.progress() == 0) {
+        timeLineMenu.play();
+      } else if (timeLineMenu.progress() === 1) {
+        timeLineMenu.reverse(0);
       }
     }
   });
@@ -113,10 +68,10 @@ const Header: React.FC = () => {
           <Link href={'/'}>
             <LogoMary width={'61'} height={'39'} />
           </Link>
-          <ButtonMenu onClick={openMenu} />
+          <ButtonMenu timeline={timeLineMenu} onClick={openMenu} />
         </NavigationContainer>
-        <Menu />
       </HeaderContainer>
+      <Menu timeline={timeLineMenu} />
     </>
   );
 };
